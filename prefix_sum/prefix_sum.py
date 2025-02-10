aa37b5b8cdf1f753f72bb60e1d13b0f4988336e6
VictoriaKGN <koganv@myumanitoba.ca>
2025-02-09 21:11:18 -0500

    prefix sum skeleton

---
+++ prefix_sum/prefix_sum.py
@@ -0,0 +1,10 @@
+
+def prefix_sum(arr):
+  prefix_sum = [0] * len(arr)
+
+  prefix_sum[0] = arr[0]
+
+  for i in range(1, len(arr)):
+      prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+  return prefix_sum
